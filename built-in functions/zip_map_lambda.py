9504c24ca1078759930bd30e61b670e08d2c3159
博弈 <16007005@qq.com>
2020-02-21 10:41:17 +0800

    python的内置函数

---
+++ built-in functions/zip_map_lambda.py
@@ -0,0 +1,43 @@
+"""
+zip(seqa, [seqb, [...]])
+zip（）接受一个或多个序列并将它们按列的元素编织在一起，当最短的顺序用完时，编织停止。
+list(zip())返回一个真实的Python列表[]。
+"""
+# 例子：
+a = (1, 2, 3, 4)
+b = (5, 6, 7, 8)
+c = (9, 10, 11)
+d = (12, 13)
+print(list(zip(a, b)))
+# 输出：[(1, 5), (2, 6), (3, 7), (4, 8)]
+print(list(zip(a, d)))
+# 输出：[(1, 12), (2, 13)]
+print(list(zip(a, b, c, d)))
+# 输出：[(1, 5, 9, 12), (2, 6, 10, 13)]
+
+"""
+请注意，当序列长度相同时，zip（）是可逆的：
+"""
+# 例子：
+a = (1, 2, 3)
+b = (4, 5, 6)
+x = zip(a, b)
+print(list(zip(a, b)))
+# 输出：[(1, 4), (2, 5), (3, 6)]
+y = zip(*x)  # 或者, apply(zip, x)
+print(list(y))
+# 输出：[(1, 2, 3), (4, 5, 6)]
+z = zip(*zip(*zip(a, b)))  # 或者, apply(zip, y)
+print(list(z))
+# 输出：[(1, 4), (2, 5), (3, 6)]
+print(list(x) == list(z))
+# 输出：True
+"""
+以下是将csv文件的行列进行倒换
+"""
+import pandas as pd
+df = pd.read_csv(r".\Data-Structures-and-Algorithms\built-in functions\weather.csv")
+data = df.values  # data是数组，直接从文件读出来的数据格式是数组
+data = list(map(list, zip(*data)))  # map()可以单独列出列表，将数组转换成列表
+data = pd.DataFrame(data, index=['Date', 'Rain', 'High', 'Low'])  # 将data的行列转换
+data.to_csv(r'.\Data-Structures-and-Algorithms\built-in functions\weather1.csv', header=0)
