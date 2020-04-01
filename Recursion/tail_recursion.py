d29d91aa219bea9cb397960132e6bfeb77d17578
LinXuDong <57325127+pccode21@users.noreply.github.com>
2020-04-01 17:11:28 +0800

    :bee:

--- Recursion/tail_recursion.py
+++ Recursion/tail_recursion.py
@@ -1,14 +1,17 @@
+"""
+使用尾递归求 1+2+3+4+5+...+n=(1+n)*n/2 的值
+"""
 def tail_recursion(n, total=0):
     if n == 0:
         return total
     else:
         return tail_recursion(n-1, total + n)
 
+
 def main():
-    n = 5
-    for i in range(n, -1, -1):
-        total = tail_recursion(n-1, total + n)
-        print('tail_recursion(%s, %s)' % (i, total))
+    n = int(input('Please input a number:'))
+    result = tail_recursion(n, 0)
+    print('This number sum is :', result)
 
 
 if __name__ == '__main__':
