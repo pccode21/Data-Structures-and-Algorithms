6357994ac2a112a366a564c7dbdf4f5c11efa7af
LinXuDong <57325127+pccode21@users.noreply.github.com>
2020-04-14 17:50:37 +0800

    Update enumerate_sorted.py

--- built-in functions/enumerate_sorted.py
+++ built-in functions/enumerate_sorted.py
@@ -7,7 +7,7 @@ sorted函数可以从任意序列的元素返回一个新的排好序的列表
 some_list = ['foo', 'bar', 'baz']
 mapping = {}
 # for i, v in enumerate(some_list):
-for i, v in enumerate(some_list, start=1):  # 可以设定索引从1开始
+for i, v in enumerate(some_list[::-1], start=1):  # 可以设定索引从1开始
     mapping[v] = i
 print(mapping)
 a = sorted([7, 1, 2, 6, 0, 3, 2])
